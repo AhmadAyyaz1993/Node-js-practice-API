66f48c243e4688aba3b11720d83fecdda3f150ed
Ahmad Ayyaz <ahmad.ayyaz8@gmail.com>
2016-05-14 19:43:48 +0500

    initititit

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(express.static(__dirname + '/public'));
 app.use(connect.logger('dev'));
 app.use(connect.json()); 
 app.use(connect.urlencoded());
-mongoose.connect('mongodb://ahmad1993:Pakistan123@ds023052.mongolab.com:023052/node-android');
+mongoose.connect('mongodb://ahmad1993:Pakistan123@ds023052.mlab.com:023052/node-android');
+//mongodb://<dbuser>:<dbpassword>@ds023052.mlab.com:23052/node-android
 // Routes
 
 require('./routes/routes.js')(app);
